58ff924c0dd65e432c441b9e0c9367933da4fd94
mojahige <mojahige@gmail.com>
2021-12-23 13:48:14 +0900

    Hello!!
    https://railsguides.jp/getting_started.html#rails%E3%81%A7%E3%80%8Chello%E3%80%8D%E3%81%A8%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /myapp/tmp/pids/server.pid
 
+# サーバーを起動する。
+bundle exec bin/rails s -p 3000 -b '0.0.0.0'
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
